fbfe7b980c8a52e48a95a45c5b6e50ec0b398663
Ryan Kulp <ryanckulp@gmail.com>
2022-02-16 16:13:13 +0900

    example blog

--- examples/pjvault.rb
+++ examples/pjvault.rb
@@ -1,24 +1,47 @@
 #!/usr/bin/env ruby
 
-# This script was contributed by Christian Neukirchen, thanks!
-
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 
 require 'feedalizer'
 require 'time'
 
-feedalize('http://pjvault.com/news77.html') do
-  feed.title = 'Pearl Jam Vault'
-  feed.description = 'Pearl Jam News and Reviews'
+# demo v2
+url = 'https://blog.useproof.com'
+feedalize(url) do
+  rss_items = []
 
-  scrape_items('//LI') do |rss_item, html_element|
-    link = html_element.xpath('a').first
+  feed.title = 'Proof Blog'
+  feed.description = ''
+
+  fd.scrape_items('//article') do |rss_item, html_element|
+    link = html_element.xpath('a').last
+    # next unless link
+    # title = link.attributes.select {|attr| attr.name == 'aria-label'}.first.value
 
     rss_item.link  = link.get('href')
-    rss_item.title = link.inner_text
+    rss_item.title = link.get('aria-label')
 
-    rss_item.description = html_element.inner_text
+    description = html_element.xpath('p').first
+    next if description.nil? || description.inner_text.nil?
+
+    rss_item.description = description.inner_text
+    rss_items << rss_item
   end
 
-  output!
+  # output!
+  rss_items
+end
+
+# demo v1
+feed = {}
+num = 1
+url = 'https://blog.useproof.com'
+fd = Feedalizer::Feed.new(url)
+fd.scrape_items('//article') do |rss_item, html_element|
+  feed[num.to_s] = {
+    rss_item: rss_item,
+    html_element: html_element
+  }
+
+  num += 1
 end
